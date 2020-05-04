9190718feb2320320f8e65339b061e474b0a8310
josephgrech <josephgrech90@gmail.com>
2020-05-04 20:50:58 +0200

    Added service layer logging.

--- src/main/java/com/jg/gopractical/service/impl/IpAddressServiceImpl.java
+++ src/main/java/com/jg/gopractical/service/impl/IpAddressServiceImpl.java
@@ -10,6 +10,7 @@ import com.jg.gopractical.service.IpAddressService;
 import com.jg.gopractical.utils.IPUtils;
 import inet.ipaddr.ipv4.IPv4Address;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ import static com.jg.gopractical.domain.enums.IpState.BLACKLISTED;
 import static com.jg.gopractical.domain.enums.IpState.RESERVED;
 import static com.jg.gopractical.domain.exception.ErrorCode.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class IpAddressServiceImpl implements IpAddressService {
@@ -30,20 +32,24 @@ public class IpAddressServiceImpl implements IpAddressService {
 
     @Override
     public IpAddress getIpAddressById(final UUID ipAddressId) {
+        log.info("Retrieving IP Address by ID '{}'.", ipAddressId);
         return ipAddressRepository.findById(ipAddressId).orElseThrow(() -> new RuntimeException("IP Address not found."));
     }
 
     @Override
     public List<IpAddress> reserveDynamicIpAddress(final Integer quantity) {
+        log.info("Reserving {} Dynamic IPs.", quantity);
         final IpPool dynamicIpPool = ipPoolRepository.findBySupportDynamicIsTrue().orElseThrow(() -> new RuntimeException("Pool not found."));
 
         if(dynamicIpPool.getAvailableCapacity() < quantity) {
+            log.warn("Dynamic Pool has only {} remaining resources.", dynamicIpPool.getAvailableCapacity());
             throw new BaseException(IP_POOL_EXHAUSTED);
         }
 
         final List<IpAddress> generatedAddresses = new ArrayList<>();
         int last = IPUtils.getIpNumbers(dynamicIpPool.getLowerBound());
 
+        log.debug("Generating Dynamic IPS...");
         for (int i = 0; i < quantity; i++) {
             final int upper = IPUtils.getIpNumbers(dynamicIpPool.getUpperBound());
 
@@ -69,39 +75,50 @@ public class IpAddressServiceImpl implements IpAddressService {
 
     @Override
     public IpAddress reserveIpAddress(final UUID poolId, final IpAddress ipAddress) {
+        log.info("Reserving IP '{}' from Pool with ID '{}'.", ipAddress.getValue(), poolId);
         ipAddress.setResourceState(RESERVED);
         final IpPool ipPool = ipPoolRepository.findById(poolId).orElseThrow(() -> new BaseException(IP_POOL_NOT_FOUND));
 
         if(ipPool.getAvailableCapacity() < 1) {
+            log.warn("IP Pool with ID '{}' has no resources left.", poolId);
             throw new BaseException(IP_POOL_EXHAUSTED);
         }
 
         if(!ipAddressInPoolRange(ipPool, ipAddress.getValue())) {
+            log.warn("IP '{}' is not in range of IP Pool with Lower Bound '{}', and Upper Bound '{}'.", ipAddress.getValue(), ipPool.getLowerBound(), ipPool.getUpperBound());
             throw new BaseException(IP_ADDRESS_NOT_IN_RANGE);
         }
 
-        if (ipAddressRepository.findByValue(ipAddress.getValue()).isPresent()) {
-            if(ipAddressRepository.findByValue(ipAddress.getValue()).get().getResourceState().equals(BLACKLISTED)){
+        final Optional<IpAddress> ipAddressByValue = ipAddressRepository.findByValue(ipAddress.getValue());
+        if (ipAddressByValue.isPresent()) {
+            if(ipAddressByValue.get().getResourceState().equals(BLACKLISTED)){
+                log.warn("IP Address is Blacklisted.");
                 throw new BaseException(IP_ADDRESS_BLACKLISTED);
             }
+
+            log.warn("IP Address is already Reserved.");
             throw new BaseException(IP_ADDRESS_NOT_AVAILABLE);
         }
 
         ipAddress.setIpPool(ipPool);
+        log.debug("Persisting IP Address '{}' for IP Pool '{}'.", ipAddress.getValue(), poolId);
         return ipAddressRepository.save(ipAddress);
     }
 
     @Override
     public IpAddress blacklistIpAddress(final UUID poolId, final String ipAddress) {
+        log.info("Blacklisting IP '{}' for IP Pool with ID '{}'.", ipAddress, poolId);
         Optional<IpAddress> optionalIpAddress = ipAddressRepository.findByValue(ipAddress);
 
         optionalIpAddress.ifPresent(foundIpAddress -> {
+            log.warn("IP Address is not Free.");
             throw new BaseException(foundIpAddress.getResourceState().equals(RESERVED) ? IP_ADDRESS_IN_USE : IP_ADDRESS_BLACKLISTED);
         });
 
         final IpPool ipPool = ipPoolRepository.findById(poolId).orElseThrow(() -> new BaseException(IP_POOL_NOT_FOUND));
 
         if(!ipAddressInPoolRange(ipPool, ipAddress)) {
+            log.warn("IP Address is not in Range ");
             throw new BaseException(IP_ADDRESS_NOT_IN_RANGE);
         }
 
@@ -117,6 +134,7 @@ public class IpAddressServiceImpl implements IpAddressService {
         final IpPool ipPool = ipPoolRepository.findById(poolId).orElseThrow(() -> new BaseException(IP_POOL_NOT_FOUND));
 
         if(!ipAddressInPoolRange(ipPool, ipAddress)) {
+            log.warn("IP '{}' is not in range of IP Pool with Lower Bound '{}', and Upper Bound '{}'.", ipAddress, ipPool.getLowerBound(), ipPool.getUpperBound());
             throw new BaseException(IP_ADDRESS_NOT_IN_RANGE);
         }
 
@@ -126,6 +144,7 @@ public class IpAddressServiceImpl implements IpAddressService {
 
     @Override
     public IpAddress getIpAddressByValue(final String ipAddress) {
+        log.debug("Retrieving IP Address object by value '{}'.", ipAddress);
         return ipAddressRepository.findByValue(ipAddress).orElseThrow(() -> new BaseException(IP_ADDRESS_NOT_FOUND));
     }
 
@@ -137,11 +156,13 @@ public class IpAddressServiceImpl implements IpAddressService {
 
     private void validateStateTransition(final IpAddress ipAddress, final IpState newState) {
         if(!ipAddress.getResourceState().getNextSteps().contains(newState)) {
+            log.warn("Cannot transit from {} to {}.", ipAddress.getResourceState(), newState);
             throw new BaseException(IP_ADDRESS_INVALID_STATE_TRANSITION);
         }
     }
 
     private boolean ipAddressInPoolRange(final IpPool ipPool, final String value) {
+        log.debug("Checking if '{}' is in range of IP Pool with ID '{}'", value, ipPool.getId());
         final IPv4Address iPv4Address = IPUtils.fromString(value);
         final IPv4Address lowerBound = IPUtils.fromString(ipPool.getLowerBound());
         final IPv4Address upperBound = IPUtils.fromString(ipPool.getUpperBound());
