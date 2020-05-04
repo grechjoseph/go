4941a9a8d17dab2eb0b9e8ea118513def9b4a29c
josephgrech <josephgrech90@gmail.com>
2020-05-04 13:12:20 +0200

    Init.

---
+++ src/main/java/com/jg/gopractical/GoPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.jg.gopractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoPracticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jg/gopractical/GoPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.gopractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
